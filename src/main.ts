3c9654b23066c67d3a612342231776b514863431
Nazar Ignatenko <nazar.ignatenkoru@gmail.com>
2025-02-19 20:54:55 +0300

    imported pinia

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import naive from 'naive-ui';
+import { createPinia } from 'pinia'
 
 const app = createApp(App);
+const pinia = createPinia()
+
 app.use(naive);
+app.use(pinia);
 app.mount('#app');
